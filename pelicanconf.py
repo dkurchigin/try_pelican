8246db774d9f43c772ede5ccdc2350bde3fec3ba
dkurchigin <kurchigin.dmitry@yandex.ru>
2023-07-16 17:13:17 +0300

    started with pelican

---
+++ pelicanconf.py
@@ -0,0 +1,19 @@
+AUTHOR = 'dkurchigin'
+SITENAME = 'Типичный Python разработчик'
+SITESUBTITLE = 'О программировании, мелочах жизни и многом другом...'
+SITEURL = ''
+
+PATH = 'content'
+TIMEZONE = 'Europe/Moscow'
+DEFAULT_LANG = 'Russian'
+
+TELEGRAM_USERNAME = 'dirty_rotten_imb'
+GITHUB_USERNAME = 'dkurchigin'
+EMAIL = 'kurchigin.dmitry@yandex.ru'
+
+
+DEFAULT_PAGINATION = 10
+THEME = "."
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
